92c874d79c6a8ef439089e6d571f3a415d5b4811
Kwikon <58586946+Kwikon@users.noreply.github.com>
2022-01-17 15:08:48 +0100

    Add files via upload

---
+++ KolejkaMikolaj/KolejkaMikolaj/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿
+namespace KolejkaMikolaj
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.osobaPoDataBadania = new System.Windows.Forms.TextBox();
+            this.osobaTerazDataBadania = new System.Windows.Forms.TextBox();
+            this.osobaPrzedDataBadania = new System.Windows.Forms.TextBox();
+            this.dodawaniaPacjentaDataBadania = new System.Windows.Forms.TextBox();
+            this.osobaPoNazwaBadania = new System.Windows.Forms.TextBox();
+            this.osobaTerazNazwaBadania = new System.Windows.Forms.TextBox();
+            this.osobaPrzedNazwaBadania = new System.Windows.Forms.TextBox();
+            this.dodawaniaPacjentaNazwaBadania = new System.Windows.Forms.TextBox();
+            this.osobaPoImie = new System.Windows.Forms.TextBox();
+            this.osobaTerazImie = new System.Windows.Forms.TextBox();
+            this.osobaPrzedImie = new System.Windows.Forms.TextBox();
+            this.dodawaniaPacjentaImie = new System.Windows.Forms.TextBox();
+            this.dodawaniePacjentaText = new System.Windows.Forms.Label();
+            this.osobaPrzedText = new System.Windows.Forms.Label();
+            this.osobaTerazText = new System.Windows.Forms.Label();
+            this.osobaPoText = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.dataTextBox = new System.Windows.Forms.TextBox();
+            this.godzinaTextBox = new System.Windows.Forms.TextBox();
+            this.dodawaniePacjentaGuzik = new System.Windows.Forms.Button();
+            this.osobaPrzedGuzik = new System.Windows.Forms.Button();
+            this.osobaTerazGuzik = new System.Windows.Forms.Button();
+            this.osobaPoGuzik = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // osobaPoDataBadania
+            // 
+            this.osobaPoDataBadania.Location = new System.Drawing.Point(452, 116);
+            this.osobaPoDataBadania.Name = "osobaPoDataBadania";
+            this.osobaPoDataBadania.Size = new System.Drawing.Size(100, 23);
+            this.osobaPoDataBadania.TabIndex = 23;
+            this.osobaPoDataBadania.TextChanged += new System.EventHandler(this.osobaPoDataBadania_TextChanged);
+            // 
+            // osobaTerazDataBadania
+            // 
+            this.osobaTerazDataBadania.Location = new System.Drawing.Point(452, 86);
+            this.osobaTerazDataBadania.Name = "osobaTerazDataBadania";
+            this.osobaTerazDataBadania.Size = new System.Drawing.Size(100, 23);
+            this.osobaTerazDataBadania.TabIndex = 22;
+            this.osobaTerazDataBadania.TextChanged += new System.EventHandler(this.osobaTerazDataBadania_TextChanged);
+            // 
+            // osobaPrzedDataBadania
+            // 
+            this.osobaPrzedDataBadania.Location = new System.Drawing.Point(452, 57);
+            this.osobaPrzedDataBadania.Name = "osobaPrzedDataBadania";
+            this.osobaPrzedDataBadania.Size = new System.Drawing.Size(100, 23);
+            this.osobaPrzedDataBadania.TabIndex = 21;
+            this.osobaPrzedDataBadania.TextChanged += new System.EventHandler(this.osobaPrzedDataBadania_TextChanged);
+            // 
+            // dodawaniaPacjentaDataBadania
+            // 
+            this.dodawaniaPacjentaDataBadania.Location = new System.Drawing.Point(452, 28);
+            this.dodawaniaPacjentaDataBadania.Name = "dodawaniaPacjentaDataBadania";
+            this.dodawaniaPacjentaDataBadania.Size = new System.Drawing.Size(100, 23);
+            this.dodawaniaPacjentaDataBadania.TabIndex = 20;
+            this.dodawaniaPacjentaDataBadania.Text = "Data Badania";
+            this.dodawaniaPacjentaDataBadania.TextChanged += new System.EventHandler(this.dodawaniaPacjentaDataBadania_TextChanged);
+            // 
+            // osobaPoNazwaBadania
+            // 
+            this.osobaPoNazwaBadania.Location = new System.Drawing.Point(306, 116);
+            this.osobaPoNazwaBadania.Name = "osobaPoNazwaBadania";
+            this.osobaPoNazwaBadania.Size = new System.Drawing.Size(100, 23);
+            this.osobaPoNazwaBadania.TabIndex = 19;
+            this.osobaPoNazwaBadania.TextChanged += new System.EventHandler(this.osobaPoNazwaBadania_TextChanged);
+            // 
+            // osobaTerazNazwaBadania
+            // 
+            this.osobaTerazNazwaBadania.Location = new System.Drawing.Point(306, 86);
+            this.osobaTerazNazwaBadania.Name = "osobaTerazNazwaBadania";
+            this.osobaTerazNazwaBadania.Size = new System.Drawing.Size(100, 23);
+            this.osobaTerazNazwaBadania.TabIndex = 18;
+            this.osobaTerazNazwaBadania.TextChanged += new System.EventHandler(this.osobaTerazNazwaBadania_TextChanged);
+            // 
+            // osobaPrzedNazwaBadania
+            // 
+            this.osobaPrzedNazwaBadania.Location = new System.Drawing.Point(306, 57);
+            this.osobaPrzedNazwaBadania.Name = "osobaPrzedNazwaBadania";
+            this.osobaPrzedNazwaBadania.Size = new System.Drawing.Size(100, 23);
+            this.osobaPrzedNazwaBadania.TabIndex = 17;
+            this.osobaPrzedNazwaBadania.TextChanged += new System.EventHandler(this.osobaPrzedNazwaBadania_TextChanged);
+            // 
+            // dodawaniaPacjentaNazwaBadania
+            // 
+            this.dodawaniaPacjentaNazwaBadania.Location = new System.Drawing.Point(306, 28);
+            this.dodawaniaPacjentaNazwaBadania.Name = "dodawaniaPacjentaNazwaBadania";
+            this.dodawaniaPacjentaNazwaBadania.Size = new System.Drawing.Size(100, 23);
+            this.dodawaniaPacjentaNazwaBadania.TabIndex = 16;
+            this.dodawaniaPacjentaNazwaBadania.Text = "Nazwa Badania";
+            this.dodawaniaPacjentaNazwaBadania.TextChanged += new System.EventHandler(this.dodawaniaPacjentaNazwaBadania_TextChanged);
+            // 
+            // osobaPoImie
+            // 
+            this.osobaPoImie.Location = new System.Drawing.Point(159, 116);
+            this.osobaPoImie.Name = "osobaPoImie";
+            this.osobaPoImie.Size = new System.Drawing.Size(100, 23);
+            this.osobaPoImie.TabIndex = 15;
+            this.osobaPoImie.TextChanged += new System.EventHandler(this.osobaPoImie_TextChanged);
+            // 
+            // osobaTerazImie
+            // 
+            this.osobaTerazImie.Location = new System.Drawing.Point(159, 86);
+            this.osobaTerazImie.Name = "osobaTerazImie";
+            this.osobaTerazImie.Size = new System.Drawing.Size(100, 23);
+            this.osobaTerazImie.TabIndex = 14;
+            this.osobaTerazImie.TextChanged += new System.EventHandler(this.osobaTerazImie_TextChanged);
+            // 
+            // osobaPrzedImie
+            // 
+            this.osobaPrzedImie.Location = new System.Drawing.Point(159, 57);
+            this.osobaPrzedImie.Name = "osobaPrzedImie";
+            this.osobaPrzedImie.Size = new System.Drawing.Size(100, 23);
+            this.osobaPrzedImie.TabIndex = 13;
+            this.osobaPrzedImie.TextChanged += new System.EventHandler(this.osobaPrzedImie_TextChanged);
+            // 
+            // dodawaniaPacjentaImie
+            // 
+            this.dodawaniaPacjentaImie.Location = new System.Drawing.Point(159, 28);
+            this.dodawaniaPacjentaImie.Name = "dodawaniaPacjentaImie";
+            this.dodawaniaPacjentaImie.Size = new System.Drawing.Size(100, 23);
+            this.dodawaniaPacjentaImie.TabIndex = 12;
+            this.dodawaniaPacjentaImie.Text = "Imię";
+            this.dodawaniaPacjentaImie.TextChanged += new System.EventHandler(this.dodawaniaPacjentaImie_TextChanged);
+            // 
+            // dodawaniePacjentaText
+            // 
+            this.dodawaniePacjentaText.AutoSize = true;
+            this.dodawaniePacjentaText.Location = new System.Drawing.Point(39, 31);
+            this.dodawaniePacjentaText.Name = "dodawaniePacjentaText";
+            this.dodawaniePacjentaText.Size = new System.Drawing.Size(114, 15);
+            this.dodawaniePacjentaText.TabIndex = 24;
+            this.dodawaniePacjentaText.Text = "Dodawanie Pacjenta";
+            this.dodawaniePacjentaText.Click += new System.EventHandler(this.dodawaniePacjentaText_Click);
+            // 
+            // osobaPrzedText
+            // 
+            this.osobaPrzedText.AutoSize = true;
+            this.osobaPrzedText.Location = new System.Drawing.Point(39, 60);
+            this.osobaPrzedText.Name = "osobaPrzedText";
+            this.osobaPrzedText.Size = new System.Drawing.Size(73, 15);
+            this.osobaPrzedText.TabIndex = 25;
+            this.osobaPrzedText.Text = "Osoba Przed";
+            this.osobaPrzedText.Click += new System.EventHandler(this.osobaPrzedText_Click);
+            // 
+            // osobaTerazText
+            // 
+            this.osobaTerazText.AutoSize = true;
+            this.osobaTerazText.Location = new System.Drawing.Point(39, 89);
+            this.osobaTerazText.Name = "osobaTerazText";
+            this.osobaTerazText.Size = new System.Drawing.Size(70, 15);
+            this.osobaTerazText.TabIndex = 26;
+            this.osobaTerazText.Text = "Osoba Teraz";
+            this.osobaTerazText.Click += new System.EventHandler(this.osobaTerazText_Click);
+            // 
+            // osobaPoText
+            // 
+            this.osobaPoText.AutoSize = true;
+            this.osobaPoText.Location = new System.Drawing.Point(39, 119);
+            this.osobaPoText.Name = "osobaPoText";
+            this.osobaPoText.Size = new System.Drawing.Size(58, 15);
+            this.osobaPoText.TabIndex = 27;
+            this.osobaPoText.Text = "Osoba Po";
+            this.osobaPoText.Click += new System.EventHandler(this.osobaPoText_Click);
+            // 
+            // dataTextBox
+            // 
+            this.dataTextBox.Location = new System.Drawing.Point(688, 28);
+            this.dataTextBox.Name = "dataTextBox";
+            this.dataTextBox.Size = new System.Drawing.Size(100, 23);
+            this.dataTextBox.TabIndex = 28;
+            this.dataTextBox.TextChanged += new System.EventHandler(this.dataTextBox_TextChanged);
+            // 
+            // godzinaTextBox
+            // 
+            this.godzinaTextBox.Location = new System.Drawing.Point(688, 56);
+            this.godzinaTextBox.Name = "godzinaTextBox";
+            this.godzinaTextBox.Size = new System.Drawing.Size(100, 23);
+            this.godzinaTextBox.TabIndex = 29;
+            this.godzinaTextBox.TextChanged += new System.EventHandler(this.godzinaTextBox_TextChanged);
+            // 
+            // dodawaniePacjentaGuzik
+            // 
+            this.dodawaniePacjentaGuzik.Location = new System.Drawing.Point(559, 28);
+            this.dodawaniePacjentaGuzik.Name = "dodawaniePacjentaGuzik";
+            this.dodawaniePacjentaGuzik.Size = new System.Drawing.Size(75, 23);
+            this.dodawaniePacjentaGuzik.TabIndex = 30;
+            this.dodawaniePacjentaGuzik.Text = "Dodaj";
+            this.dodawaniePacjentaGuzik.UseVisualStyleBackColor = true;
+            // 
+            // osobaPrzedGuzik
+            // 
+            this.osobaPrzedGuzik.Location = new System.Drawing.Point(558, 57);
+            this.osobaPrzedGuzik.Name = "osobaPrzedGuzik";
+            this.osobaPrzedGuzik.Size = new System.Drawing.Size(75, 23);
+            this.osobaPrzedGuzik.TabIndex = 31;
+            this.osobaPrzedGuzik.Text = "Cofnij";
+            this.osobaPrzedGuzik.UseVisualStyleBackColor = true;
+            // 
+            // osobaTerazGuzik
+            // 
+            this.osobaTerazGuzik.Location = new System.Drawing.Point(559, 86);
+            this.osobaTerazGuzik.Name = "osobaTerazGuzik";
+            this.osobaTerazGuzik.Size = new System.Drawing.Size(75, 23);
+            this.osobaTerazGuzik.TabIndex = 32;
+            this.osobaTerazGuzik.Text = "Usuń";
+            this.osobaTerazGuzik.UseVisualStyleBackColor = true;
+            // 
+            // osobaPoGuzik
+            // 
+            this.osobaPoGuzik.Location = new System.Drawing.Point(558, 116);
+            this.osobaPoGuzik.Name = "osobaPoGuzik";
+            this.osobaPoGuzik.Size = new System.Drawing.Size(75, 23);
+            this.osobaPoGuzik.TabIndex = 33;
+            this.osobaPoGuzik.Text = "Dalej";
+            this.osobaPoGuzik.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.osobaPoGuzik);
+            this.Controls.Add(this.osobaTerazGuzik);
+            this.Controls.Add(this.osobaPrzedGuzik);
+            this.Controls.Add(this.dodawaniePacjentaGuzik);
+            this.Controls.Add(this.godzinaTextBox);
+            this.Controls.Add(this.dataTextBox);
+            this.Controls.Add(this.osobaPoText);
+            this.Controls.Add(this.osobaTerazText);
+            this.Controls.Add(this.osobaPrzedText);
+            this.Controls.Add(this.dodawaniePacjentaText);
+            this.Controls.Add(this.osobaPoDataBadania);
+            this.Controls.Add(this.osobaTerazDataBadania);
+            this.Controls.Add(this.osobaPrzedDataBadania);
+            this.Controls.Add(this.dodawaniaPacjentaDataBadania);
+            this.Controls.Add(this.osobaPoNazwaBadania);
+            this.Controls.Add(this.osobaTerazNazwaBadania);
+            this.Controls.Add(this.osobaPrzedNazwaBadania);
+            this.Controls.Add(this.dodawaniaPacjentaNazwaBadania);
+            this.Controls.Add(this.osobaPoImie);
+            this.Controls.Add(this.osobaTerazImie);
+            this.Controls.Add(this.osobaPrzedImie);
+            this.Controls.Add(this.dodawaniaPacjentaImie);
+            this.Name = "Form1";
+            this.Text = "-";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox osobaPoDataBadania;
+        private System.Windows.Forms.TextBox osobaTerazDataBadania;
+        private System.Windows.Forms.TextBox osobaPrzedDataBadania;
+        private System.Windows.Forms.TextBox dodawaniaPacjentaDataBadania;
+        private System.Windows.Forms.TextBox osobaPoNazwaBadania;
+        private System.Windows.Forms.TextBox osobaTerazNazwaBadania;
+        private System.Windows.Forms.TextBox osobaPrzedNazwaBadania;
+        private System.Windows.Forms.TextBox dodawaniaPacjentaNazwaBadania;
+        private System.Windows.Forms.TextBox osobaPoImie;
+        private System.Windows.Forms.TextBox osobaTerazImie;
+        private System.Windows.Forms.TextBox osobaPrzedImie;
+        private System.Windows.Forms.TextBox dodawaniaPacjentaImie;
+        private System.Windows.Forms.Label dodawaniePacjentaText;
+        private System.Windows.Forms.Label osobaPrzedText;
+        private System.Windows.Forms.Label osobaTerazText;
+        private System.Windows.Forms.Label osobaPoText;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TextBox dataTextBox;
+        private System.Windows.Forms.TextBox godzinaTextBox;
+        private System.Windows.Forms.Button dodawaniePacjentaGuzik;
+        private System.Windows.Forms.Button osobaPrzedGuzik;
+        private System.Windows.Forms.Button osobaTerazGuzik;
+        private System.Windows.Forms.Button osobaPoGuzik;
+    }
+}
+
---
+++ KolejkaMikolaj/KolejkaMikolaj/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KolejkaMikolaj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataTextBox.Text = DateTime.Now.Date.ToShortDateString();
+            godzinaTextBox.Text = DateTime.Now.ToString("hh:mm:ss");
+        }
+
+        private void dodawaniePacjentaText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void osobaPrzedText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void osobaTerazText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void osobaPoText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dodawaniaPacjentaImie_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void osobaPrzedImie_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void osobaTerazImie_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void osobaPoImie_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dodawaniaPacjentaNazwaBadania_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void osobaPrzedNazwaBadania_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void osobaTerazNazwaBadania_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void osobaPoNazwaBadania_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dodawaniaPacjentaDataBadania_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void osobaPrzedDataBadania_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void osobaTerazDataBadania_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void osobaPoDataBadania_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void godzinaTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            godzinaTextBox.Text = DateTime.Now.ToString("hh:mm:ss");
+
+
+        }
+    }
+}
